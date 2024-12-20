21628a69e868c9bd033b82a7c250858df14b0d6f
ALJDBS <70330110+TheSpawnal@users.noreply.github.com>
2024-12-20 19:45:40 +0100

    Update exam24_23.scala
    asw

--- exam24/exam24_23.scala
+++ exam24/exam24_23.scala
@@ -9,9 +9,11 @@ def transformPrinter(f: Printer[Bird] => List[Bird]) : Unit = ()
 
 which of the following calls will not give a type error ? 1 or more
 
-transformPrinter(null: Printer[Nothing]=>List[Nothing])<-
+transformPrinter(null: Printer[Nothing]=>List[Nothing])
 transformPrinter(null: Printer[Ostrich]=> List[Animal])
 transformPrinter(null: Printer[Animal]=>List[Animal])
-transformPrinter(null: Printer[Ostrich]=>List[Ostrich])<-
-transformPrinter(null: Any => Nothing)<-
+transformPrinter(null: Printer[Ostrich]=>List[Ostrich])
+transformPrinter(null: Any => Nothing)
 transformPrinter(null: Printer[Animal]=>List[Ostrich])
+
+(A,D,E)
