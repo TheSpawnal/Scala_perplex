ffdb793b3f7821839bc071bc1ee23676ebd194df
ALJDBS <70330110+TheSpawnal@users.noreply.github.com>
2024-12-20 19:53:36 +0100

    Update exam24_21.scala
    asw

--- exam24/exam24_21.scala
+++ exam24/exam24_21.scala
@@ -7,4 +7,6 @@ what is the desugared form of this for loop ?
 List(1,2,3).map(x=>List(4,5).map(y=>x*y))
 List(1,2,3).map(x=>List(4,5).flatMap(y=>x *y))
 List(4,5).flatMap(y=>List(1,2,3).map(x=>x*y))
-List(1,2,3).flatMap(x=>List(4,5).map(y => x * y)) <-
+List(1,2,3).flatMap(x=>List(4,5).map(y => x * y)) 
+
+(D)
