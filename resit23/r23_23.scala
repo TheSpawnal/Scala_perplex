cdaf1a4eca5a7c3512f898acbec9bdf4d35cc084
ALJDBS <70330110+TheSpawnal@users.noreply.github.com>
2024-12-28 19:14:37 +0100

    Create r23_23.scala

---
+++ resit23/r23_23.scala
@@ -0,0 +1,16 @@
+class List[+A]
+class MutVar[A]
+class Animal
+class Bird extends Animal
+class Ostrich extends Bird
+
+Which of the following are subtypes of Mutvar[Bird] => List[Bird] ? Choose 1 or more
+
+ A MutVar[Ostrich] => List[Animal]
+ B Any => List[Nothing]
+ C MutVar[Bird] => List[Ostrich]
+ D Any => Nothing
+ E MutVar[Any] => List[Ostrich]
+ F MutVar[Animal] => List[Ostrich]
+
+(C,B,D)
