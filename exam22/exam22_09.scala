bbcd569dfb677cf1c19356ef28640a39836ece91
ALJDBS <70330110+TheSpawnal@users.noreply.github.com>
2024-12-29 19:43:35 +0100

    Update exam22_09.scala

--- exam22/exam22_09.scala
+++ exam22/exam22_09.scala
@@ -4,5 +4,7 @@ Which of the following statements about LISP are true?
  A LISP was introduced after 2000
  B LISP does not use a garbage collector
  C LISP is often used to program triple-A videogames
- D In LISP, code is data <-
+ D In LISP, code is data 
  E LISP is used often as an alternative to C++
+
+(D)
