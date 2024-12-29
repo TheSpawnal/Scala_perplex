1c77bdf645b9764e536c2e75343ed53001852efd
ALJDBS <70330110+TheSpawnal@users.noreply.github.com>
2024-12-29 19:34:27 +0100

    Update exam22_06.scala
    hs

--- exam22/exam22_06.scala
+++ exam22/exam22_06.scala
@@ -8,9 +8,11 @@ class C extends B
  Given the above declarations, which of the following types are subtypes of
  Array[C => A] ? Select 0 or more
 
- A Nothing <-
+ A Nothing 
  B Array[A => C]
  C Array[B => B]
  D Array[A => A]
  E Array[Any => Nothing]
  F Array[C => C]
+
+(A)
