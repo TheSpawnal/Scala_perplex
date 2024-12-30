72dda29e6993baf8f765bcf5cc64863799ccd734
ALJDBS <70330110+TheSpawnal@users.noreply.github.com>
2024-12-30 20:58:33 +0100

    Update exam22_22.scala

--- exam22/exam22_22.scala
+++ exam22/exam22_22.scala
@@ -12,9 +12,12 @@ def zw[A,B,C](f: (A,B) => C) : (List[A], List[B]) => List[C] = {
 val m = List[Int] = zw((_: Int) - (_ : Int)) (List(3,2,1), List(1,2,3,4))
 
  What is the value of m?
+
  A List(1,2,3,4)
  B List(2,0,-2,-4)
- C List(2,0,-2) <-
+ C List(2,0,-2) 
  D List(3,2,1)
  E List(-2,0,2)
  F List(-2,0,2,4)
+
+(C)
