638df80b6c4fc5951f04d37f60d444e65a578def
ALJDBS <70330110+TheSpawnal@users.noreply.github.com>
2024-12-30 21:17:45 +0100

    Update exam22_27.scala

--- exam22/exam22_27.scala
+++ exam22/exam22_27.scala
@@ -11,12 +11,14 @@ def andThen[A, B, C](f: A=> B, g: B => C): A => C = x => g(f(x))
 def times2: Int => Int _ * 2
 def min1: Int => Int = _ - 1
 
-val x: Int = andThen(reit(time2, 4), reit(andThen(min1, min1), 5))(1)
+val x: Int = andThen(reit(times2, 4), reit(andThen(min1, min1), 5))(1)
 
 what is the value x ? 
 
-A 6 <-
+A 6 
 B -12
 C 0
 D 12
 E -144
+
+(A)
