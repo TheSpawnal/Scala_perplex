9d8ca13b811653f9c55d8d583b151ae03a0b4c38
ALJDBS <70330110+TheSpawnal@users.noreply.github.com>
2024-12-30 21:03:04 +0100

    Update exam22_23.scala

--- exam22/exam22_23.scala
+++ exam22/exam22_23.scala
@@ -9,9 +9,12 @@ case class GPokeball[A <: Pokemon](p: A)
 val m = (FPokeball(Slowbro).p, GPokeball(Slowbro).p)
 
 What is the (most specific) type of m?
+
  A (Slowbro, Slowbro)
  B (Pokemon, Pokemon)
  C (FPokeball, Slowbro)
  D (Slowbro, Pokemon)
  E (FPokeball, Pokemon)
- F (Pokemon,SlowBro) <- 
+ F (Pokemon,SlowBro) 
+
+(F)
