eb6bdc076f8203b5b9037d02e21bb1662ca796cd
ALJDBS <70330110+TheSpawnal@users.noreply.github.com>
2024-12-30 21:05:44 +0100

    Update exam22_24.scala
    curry Haskell

--- exam22/exam22_24.scala
+++ exam22/exam22_24.scala
@@ -3,9 +3,13 @@
 def f[A, B, C](f: (A,B) => C)(x: A)(y: B) : C = f(x, y)
 
 What is the name of this higher order function?
- A Curry <-
+
+ A Curry 
  B Compose
  C AndThen
  D Flatmap
  E Filter
  F Uncurry
+
+
+(A)
