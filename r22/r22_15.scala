b6a43826bee79f69d88aa52a5d09edad34ea473b
ALJDBS <70330110+TheSpawnal@users.noreply.github.com>
2024-12-31 19:23:13 +0100

    Create r22_15.scala

---
+++ r22/r22_15.scala
@@ -0,0 +1,16 @@
+
+
+abstract class A
+class B extends A
+class C extends B
+
+Which of the following types are subtypes of the type List[C => A]?
+Select 1 or more.
+ A Nothing
+ B List[A => C]
+ C List[B => B]
+ D List[A => A]
+ E List[Any => Nothing]
+ F List[C => C]
+
+(all)
