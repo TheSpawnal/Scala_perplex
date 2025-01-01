77ebe1e01103fde68ce60ab3631b564526132b63
ALJDBS <70330110+TheSpawnal@users.noreply.github.com>
2025-01-02 00:39:44 +0100

    Update exam21_20.scala

--- exam21/exam21_20.scala
+++ exam21/exam21_20.scala
@@ -10,9 +10,11 @@ def uncurry[A, B,C](f : A=> B => C) (x : A, y : B) : C = f(x)(y)
 Which of the following equivalences are true 
 (where :=: means is equivalent to)? (Select zero or more)
 
-A curry(tl) :=: cl @
-B uncurry(cl) :=: (x,y) => x + y @
+A curry(tl) :=: cl 
+B uncurry(cl) :=: (x,y) => x + y 
 C atl :=: uncurry(tl)
-D curry(atl) :=: curry(tl) @
+D curry(atl) :=: curry(tl) 
 E curry(uncurry(tl)) := atl
 F curry(cl) :=: x => y => x + y
+
+(A,B,D)
