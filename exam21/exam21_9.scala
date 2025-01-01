466f4e2b1dcd66ef4cbc2adc7acd76abd3961642
ALJDBS <70330110+TheSpawnal@users.noreply.github.com>
2025-01-01 23:30:16 +0100

    Update exam21_9.scala

--- exam21/exam21_9.scala
+++ exam21/exam21_9.scala
@@ -18,8 +18,10 @@ object E {
 
 Which errors will the above code give?
 
- A Cannot instantiate abstract class <-
+ A Cannot instantiate abstract class 
  B Cannot subclasss final class
- C Class E must either be declared abstract or implement abstract member <-
+ C Class E must either be declared abstract or implement abstract member 
  D Cannot instantiate final class
  E Cannot declare final method in non-final class
+
+(A,C)
